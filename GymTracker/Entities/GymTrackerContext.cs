46080c1af5a9abfe000284888dead3f5f014f5f1
Dawid Kułaga <dawidson.kiano@gmail.com>
2023-06-19 19:08:53 +0200

    Add project files.

---
+++ GymTracker/Entities/GymTrackerContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GymTracker.Entities
+{
+    public class GymTrackerContext : DbContext
+    {
+        public GymTrackerContext(DbContextOptions<GymTrackerContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+    }
+}
